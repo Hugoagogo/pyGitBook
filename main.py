677bb074762a05b6eb5029846005812245761239
Hugoagogo <Hughpowell2004@yahoo.com.au>
2011-06-27 21:30:54 +1000

    Very basics working

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-#from git import Repo
-#
-#repo = Repo("C:\Users\Hugh\Desktop\citySquare")
-#
-#tags = repo.tags
-#tagref = tags[0]
-#print tagref.tag                  # tags may have tag objects carrying additional information
-#print tagref.commit               # but they always point to commits
-#
-#
-##for x in repo.iter_commits('master', max_count=100):
-##    print x.message
-##
-##print help(git)
-
+# Use this is generate log data for the default template
 #git log --pretty="format:[START commit][author=%an][time=%at][message=%s][hash=%H]" --shortstat > out.txt
 
-import re
+import re, os, datetime
+from jinja2 import Environment, FileSystemLoader
+
+TEMPLATE_DIR = os.path.join(os.path.abspath('templates'),"github")
+
+ENV = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 
 data = re.compile(r"\[(\w+=.*?)\]")
 changes = re.compile(r"(\d+) files changed, (\d+) insertions[(][+][)], (\d+) deletions")
+
+def get_css(template):
+    css = ""
+    for fname in os.listdir(os.path.join(template,"css")):
+        fname = os.path.join(template,"css",fname)
+        if os.path.isfile(fname):
+            f = open(fname)
+            css += f.read() + "\n"
+            f.close()
+    return css
 
 f = open("out.txt")
 r_commits = [x.strip().split("\n") for x in f.read().split("\n[START commit]")]
@@ -33,10 +34,18 @@ for r_commit in r_commits:
     for item in re.findall(data,r_commit[0]):
         commit.__setattr__(*item.split("=",1))
     if len(r_commit) > 1:
-        commit.changed, commit.inserts, commit.deleats = re.search(changes,r_commit[1]).groups()
+        commit.changed, commit.inserts, commit.deletes = re.search(changes,r_commit[1]).groups()
     else:
-        commit.changed = commit.inserts = commit.deleats = "-"
+        commit.changed = commit.inserts = commit.deletes = "-"
         
     commits.append(commit)
     
-print len(commits)
+template = ENV.get_template("main.html")
+
+data = {"commits":commits,
+        "title":"This is a test title",
+        "style":get_css(TEMPLATE_DIR)}
+
+f = open("out.html","w")
+f.write(template.render(data))
+f.close()
