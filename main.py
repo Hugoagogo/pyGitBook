d5e28b64962fb5f75227e9676884359aeba83431
Hugoagogo <Hughpowell2004@yahoo.com.au>
2011-06-28 17:17:33 +1000

    Added some settings to the top of the program and updated the demo datafiles

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # Use this is generate log data for the default template
-#git log --pretty="format:[START commit][author=%an][time=%at][message=%s][hash=%H]" --shortstat > out.txt
+#git log --pretty="format:[START commit][author=%an][time=%at][message=%s][hash=%H]" --shortstat > git-data.txt
 
 import re, os, datetime
 from jinja2 import Environment, FileSystemLoader
 
+DATAFILE = "git-data.txt"
+HTMLFILE = "gitBook.html"
+HEADING = "RepoName"
 TEMPLATE_DIR = os.path.join(os.path.abspath('templates'),"github")
 
 ENV = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
@@ -11,7 +14,9 @@ ENV = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 data = re.compile(r"\[(\w+=.*?)\]")
 changes = re.compile(r"(\d+) files changed, (\d+) insertions[(][+][)], (\d+) deletions")
 
-def get_css(template):
+class Commit(object):pass ## Just a blank object
+
+def get_css(template): ## Makes a big blob of CSS so you dont need to worry about external files.
     css = ""
     for fname in os.listdir(os.path.join(template,"css")):
         fname = os.path.join(template,"css",fname)
@@ -21,13 +26,11 @@ def get_css(template):
             f.close()
     return css
 
-f = open("out.txt")
+f = open(DATAFILE)
 r_commits = [x.strip().split("\n") for x in f.read().split("\n[START commit]")]
 f.close()
 
 commits = []
-
-class Commit(object):pass
 
 for r_commit in r_commits:
     commit = Commit()
@@ -45,10 +48,12 @@ for r_commit in r_commits:
     
 template = ENV.get_template("main.html")
 
-data = {"commits":commits,
-        "title":"This is a test title",
-        "style":get_css(TEMPLATE_DIR)}
+data = {"title":RepoName,
+        "style":get_css(TEMPLATE_DIR),
+        "commits":commits}
 
-f = open("out.html","w")
+f = open(HTMLFILE,"w")
 f.write(template.render(data))
 f.close()
+
+print "All done you now have a nice logbook at:", HTMLFILE
