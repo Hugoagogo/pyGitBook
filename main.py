4a7aad01ce2b6febaa0259b694c5f4822ccbe896
Patrick Huck <phuck@lbl.gov>
2013-05-22 01:12:36 -0700

    main.py: add shebang line

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Use this is generate log data for the default template
 #git log --pretty="format:[START commit][author=%an][time=%at][message=%s][hash=%H]" --shortstat > git-data.txt
 
